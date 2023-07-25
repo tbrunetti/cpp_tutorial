4c6a7c5c1dc2217c11d5cb01aad84bba3027dbd9
tbrunetti <brunettt@colorado.edu>
2023-07-24 19:43:09 -0600

    declaring and using different variable types

--- variables_types.cpp
+++ variables_types.cpp
@@ -1,5 +1,7 @@
 #include <iostream>  //header file library
+#include <string>
 using namespace std; //use names for objects and variables from the standard library
+
 
 /* variable types:
 int - stores integers
@@ -46,4 +48,55 @@ int main() {
     const int minutesPerHour = 60;
     const float PI = 3.14;
 
-}
+    int userIn;
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> userIn; //Get user input from the keyboard
+    cout << "Your number is: " << userIn << "\n"; //Display the input value
+
+    // Create a calculator that adds two user input values
+    int in1, in2;
+    int newSum;
+    cout << "Type a number: ";
+    cin >> in1;
+    cout << "Type another number: ";
+    cin >> in2;
+    newSum = in1 + in2;
+    cout << "Sum is: " << newSum << endl;
+
+    /* data types:
+    boolean = 1 byte = true/false
+    char = 1 byte = single character/letter/number, or ASCII values
+    int = 2 or 4 bytes = whole number without decimals
+    float = 4 bytes = fractional number containing 1 or more decimals (6-7 decimal digits)
+    double = 8 bytes = fraction numbers containing 1 or more decimanls (15 decimal digits)
+    */
+
+    // scientific notation example; lowercase e and capital E can be used interchangeably to denote power of 10
+    float f1 = 35e3;
+    double d1 = 12E4;
+    cout << f1 << "\n";
+    cout << d1 << endl;
+
+    // boolean example
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun << "\n"; // Outputs 1 (true)
+    cout << isFishTasty << endl; // Outputs 0 (false)
+
+    // char example; must be a single character surrounded by single quotes '
+    char myGrade = 'B';
+    cout << myGrade << endl;
+    // char example using ASCII values
+    char d = 65, e = 66, f = 67;
+    cout << d << "\n";
+    cout << e << "\n";
+    cout << f << endl;
+
+
+
+    /* string example 
+    You must include an additiona header file in the source code to use string as it is not a 
+    built-in type: #include <string> */
+    string greeting = "Hello";
+    cout << greeting << "\n";
+    }
