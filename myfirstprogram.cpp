2fd38902a48c154f23b6eabfa482788ee12ec857
tbrunetti <brunettt@colorado.edu>
2023-07-24 18:32:26 -0600

    printing to stdout and adding newlines

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -2,10 +2,17 @@
 using namespace std; //use names for objects and variables from the standard library
 
 
-/* function called main; note all cpp code ends with semi-colon (similar to Java and ANSII C) */
+/* function called main(); 
+note all cpp code ends with semi-colon 
+(similar to Java and ANSII C) */
+
 int main() {
     cout << "Hello World!"; // cout is the oject and when paired with the insertion operator (<<) prints object to standard out 
     // std::cout << "Hello World!"; is the same as the above line without having to list the "using namespace std;"
+    cout << "I am learning C++. Notice how there is no space inserted prior to the previous print text statement!";
+    cout << "Adding a blank line now! \n\n";
+    cout << "new line with blank";
+    cout << "The following will also add a return" << endl;
     return 0;
 }
 
