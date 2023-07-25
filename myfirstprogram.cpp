36b85dd6b90271906f4be502bc02827dc9b6c9a5
tbrunetti <brunettt@colorado.edu>
2023-07-24 18:26:12 -0600

    annotated code

--- myfirstprogram.cpp
+++ myfirstprogram.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  //header file library
+using namespace std; //use names for objects and variables from the standard library
 
+
+/* function called main; note all cpp code ends with semi-colon (similar to Java and ANSII C) */
 int main() {
-    cout << "Hello World!";
+    cout << "Hello World!"; // cout is the oject and when paired with the insertion operator (<<) prints object to standard out 
+    // std::cout << "Hello World!"; is the same as the above line without having to list the "using namespace std;"
     return 0;
 }
+
